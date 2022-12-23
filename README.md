1e7b3384ba8cdb0856fde3297304934130119693
Broňa Růžička <brona.ruzicka@gmail.com>
2022-12-23 03:44:02 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,95 @@
+# Simple HTTP File Saver
+
+A simple program running an HTTP server on localhost. It saves any POST request body into a file, whose location (inside a specified output directory) is determined by the request pathname.
+
+Note: this package is designed as a utility.
+
+
+
+## Usage
+
+The simplest usage case is included below
+
+```bash
+npx simple-http-file-saver
+```
+
+
+
+## Options
+
+This script includes 3 command line options.
+
+
+### HTTP Port
+
+This option allows to change the HTTP port, on which the server is listening (`8000` by default)
+
+```bash
+npx simple-http-file-saver -p 8080
+```
+
+```bash
+npx simple-http-file-saver --port 8008
+```
+
+
+### Output directory
+
+This option allows to specify the root output directory (`./data` by default)
+
+```bash
+npx simple-http-file-saver -d ./output
+```
+```bash
+npx simple-http-file-saver --directory /home/example.user/some/directory
+```
+
+### Help
+
+This utility includes a help option.
+
+```bash
+npx simple-http-file-saver -h
+```
+```bash
+npx simple-http-file-saver --help
+```
+
+
+
+## Example
+
+The console input would look like this.
+
+```bash
+bash$ cd /home/example.user/
+bash$ npx simple-http-file-saver -d example
+
+Starting Simple HTTP File Saver:
+Listening on 'http://localhost:8000'.
+Saving to directory '/home/example.user/example'.
+
+Intiating webserver.
+Webserver running.
+```
+
+In response to the following post request 
+
+```http
+POST /a/specific/file.json HTTP/1.1
+Hostname: localhost
+Content-Length: 15
+
+text=Not_a_json
+```
+
+the script would create all the necesarry directories and the following file
+
+```
+/home/example.user/example/a/specific/file.json
+```
+ would contain this
+```json
+text=Not_a_json
+```
---
+++ index.js
@@ -0,0 +1,86 @@
+#!/usr/bin/env node
+
+const { program } = require("commander");
+
+const fs = require("fs");
+const path = require("path");
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+
+
+
+program
+    .name("npx simple-http-file-saver")
+    .description(
+        `A simple program running an HTTP server on localhost.` + `\n` + 
+        `It saves any POST request body into a file,`           + `\n` +
+        `whose location (inside a specified output directory)`  + `\n` + 
+        `is determined by the request pathname.`
+    )
+    .option(
+        "-p, --port <port>",
+        "change http <port>",
+        (text, last) => parseInt(text),
+        8000
+    )
+    .option(
+        "-d, --dir, --directory <path>",
+        "specify root output directory <path>",
+        "./data"
+    )
+    .parse()
+
+const options = program.opts();
+
+const port = options.port;
+const dataDirectory = path.resolve(options.dir ?? options.directory);
+
+
+
+console.log(``);
+console.log(`Starting Simple HTTP File Saver:`);
+console.log(`Listening on 'http://localhost:${port}'.`);
+console.log(`Saving to directory '${dataDirectory}'.`);
+console.log(``);
+
+
+
+console.log(`Intiating webserver.`);
+
+
+const app = express();
+
+app.use(bodyParser.raw({ type: "*/*" }));
+
+app.post("*", async (request, response) => {
+    
+    try {
+        
+        const requestPath = request.path.substring(1);
+        const fullPath = path.join(dataDirectory, requestPath);
+        const directoryPath = path.dirname(fullPath);
+
+        await fs.promises.mkdir(directoryPath, { recursive: true });
+        await fs.promises.writeFile(fullPath, request.body);
+
+        console.log(`${request.body.byteLength} b`.padStart(10), `|`, requestPath);
+
+        response.sendStatus(204);
+
+    } catch(e) {
+        console.error(e);
+        response.sendStatus(500);
+    }
+
+});
+
+app.all("*", (request, response) => response.sendStatus(405));
+
+
+app.listen(port, () => {
+    console.log(`Webserver running.`);
+    console.log(``);
+    console.log(``);
+});
